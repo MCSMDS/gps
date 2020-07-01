01c64654a541335f7796753ae3d3a20251e1184b
MCSMDS <mfsdmcsmds@gmail.com>
2020-07-01 17:35:43 +0800

    updata

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,12 @@ class CurrentLocationWidget extends StatefulWidget {
 }
 
 class _LocationState extends State<CurrentLocationWidget> {
-  Position _currentPosition;
+  final List<Position> _positions = <Position>[];
   Timer _timer;
 
   @override
   void initState() {
+    _initCurrentLocation();
     _timer = Timer.periodic(Duration(seconds: 5), (Timer timer) {
       _initCurrentLocation();
     });
@@ -25,68 +26,13 @@ class _LocationState extends State<CurrentLocationWidget> {
     Geolocator geolocator = Geolocator()..forceAndroidLocationManager = true;
     Position position = await geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.bestForNavigation);
-    setState(() => _currentPosition = position);
+    setState(() => _positions.add(position));
   }
 
   @override
   void dispose() {
     _timer?.cancel();
     _timer = null;
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('GPS'),
-      ),
-      body: FutureBuilder<GeolocationStatus>(
-        future: Geolocator().checkGeolocationPermissionStatus(),
-        builder:
-            (BuildContext context, AsyncSnapshot<GeolocationStatus> snapshot) {
-          if (!snapshot.hasData || _currentPosition == null) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          if (snapshot.data == GeolocationStatus.denied) {
-            return Text('请允许此应用程序访问位置。');
-          }
-
-          return Center(
-            child: Text(_currentPosition.toString()),
-          );
-        },
-      ),
-    );
-  }
-}
-
-/*
-import 'dart:async';
-
-class LocationStreamWidget extends StatefulWidget {
-  @override
-  State<LocationStreamWidget> createState() => LocationStreamState();
-}
-
-class LocationStreamState extends State<LocationStreamWidget> {
-  StreamSubscription<Position> _positionStreamSubscription;
-  final List<Position> _positions = <Position>[];
-
-  @override
-  void initState() {
-    Geolocator geolocator = Geolocator()..forceAndroidLocationManager = true;
-    Stream<Position> stream = geolocator.getPositionStream();
-    _positionStreamSubscription = stream.listen(
-        (Position position) => setState(() => _positions.add(position)));
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _positionStreamSubscription.cancel();
-    _positionStreamSubscription = null;
     super.dispose();
   }
 
@@ -105,7 +51,7 @@ class LocationStreamState extends State<LocationStreamWidget> {
         future: checkPermission(),
         builder:
             (BuildContext context, AsyncSnapshot<GeolocationStatus> snapshot) {
-          if (!snapshot.hasData) {
+          if (!snapshot.hasData || _positions.length == 0) {
             return Center(child: CircularProgressIndicator());
           }
 
@@ -139,4 +85,3 @@ class LocationStreamState extends State<LocationStreamWidget> {
     );
   }
 }
-*/
