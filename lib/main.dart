c73ddaa77ea86d0b01efc6ad12989eee361b0e58
MCSMDS <mfsdmcsmds@gmail.com>
2020-07-02 14:06:16 +0800

    updata

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,13 @@ class _LocationState extends State<CurrentLocationWidget> {
     super.initState();
   }
 
-  void addtLocation() async {
+  addtLocation() async {
     Geolocator geolocator = Geolocator()..forceAndroidLocationManager = true;
-    Position newposition = await geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.medium);
-    if (positions.length == 0 ||
-        (positions.last.longitude == newposition.longitude &&
-            positions.last.latitude == newposition.latitude &&
-            positions.last.altitude == newposition.altitude)) return;
+    Position newposition = await geolocator.getCurrentPosition();
+    if (positions.length != 0 &&
+        positions.last.longitude == newposition.longitude &&
+        positions.last.latitude == newposition.latitude &&
+        positions.last.altitude == newposition.altitude) return;
     setState(() => positions.add(newposition));
   }
 
@@ -49,7 +48,7 @@ class _LocationState extends State<CurrentLocationWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('GPS'),
+        title: const Text('GPS'),
       ),
       body: FutureBuilder<GeolocationStatus>(
         future: checkPermission(),
@@ -71,7 +70,7 @@ class _LocationState extends State<CurrentLocationWidget> {
     List<Widget> item = <Widget>[];
     item.addAll(positions.map((Position position) {
       return ListTile(
-        title: Row(
+        title: Column(
           children: <Widget>[
             Text('经度: ${dd2dms(position.longitude)}'),
             Text('纬度: ${dd2dms(position.latitude)}'),
@@ -88,6 +87,6 @@ class _LocationState extends State<CurrentLocationWidget> {
     int du = number.toInt();
     int feng = ((number - du) * 60).toInt();
     double miao = (((number - du) * 60 - feng) * 60);
-    return "${du.toString()}° ${feng.toString()}' ${miao.toStringAsFixed(1)}''";
+    return "${du.toString()}°${feng.toString()}'${miao.toStringAsFixed(1)}\"";
   }
 }
